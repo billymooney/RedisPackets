d614dd73719463b4879b932c4eeb9ebb65fcce7f
Billison <developer.d0tjar@gmail.com>
2021-12-30 22:58:10 +0000

    Update Main.java

--- src/main/java/org/jefferies/packets/example/Main.java
+++ src/main/java/org/jefferies/packets/example/Main.java
@@ -6,19 +6,12 @@ import org.jefferies.packets.handler.RedisHandler;
 @Getter
 public class Main {
 
-    @Getter private static Main instance;
-    private RedisHandler handler;
-
-    public Main(){
-        instance = this;
-        handler = new RedisHandler("Test", "127.0.0.1", 6379, "");
+    public static void main(String[] args){
+        RedisHandler handler = new RedisHandler("Test", "127.0.0.1", 6379, "");
         handler.registerPacket(new ExamplePacket());
         handler.setListener(new Listener());
         handler.sendPacket(new ExamplePacket("test"));
-    }
-
-    public static void main(String[] args){
-        new Main();
+        // Prints 'test' in each console instance where this exact code code is running.
     }
 
 }
